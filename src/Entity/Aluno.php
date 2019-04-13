18326af5e75f5b1c1091a3b560c00851b71c3502
rosental <cristiane.rosental@gmail.com>
2019-04-13 10:57:48 -0300

    adicionando entidade de aluno

---
+++ src/Entity/Aluno.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AlunoRepository")
+ */
+class Aluno
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nome;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $cpf;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $cep;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $endereco;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $telefone;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getCpf(): ?int
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(int $cpf): self
+    {
+        $this->cpf = $cpf;
+
+        return $this;
+    }
+
+    public function getCep(): ?int
+    {
+        return $this->cep;
+    }
+
+    public function setCep(int $cep): self
+    {
+        $this->cep = $cep;
+
+        return $this;
+    }
+
+    public function getEndereco(): ?string
+    {
+        return $this->endereco;
+    }
+
+    public function setEndereco(string $endereco): self
+    {
+        $this->endereco = $endereco;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTelefone(): ?int
+    {
+        return $this->telefone;
+    }
+
+    public function setTelefone(int $telefone): self
+    {
+        $this->telefone = $telefone;
+
+        return $this;
+    }
+}
