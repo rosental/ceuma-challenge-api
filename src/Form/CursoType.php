d42628ef1d34e6f946052bd48dbc3ab21b52771d
rosental <cristiane.rosental@gmail.com>
2019-04-16 00:28:34 -0300

    removendo campos que serao adicionados pelo Doctrine e pelo formulario do Symfony.

--- src/Form/CursoType.php
+++ src/Form/CursoType.php
@@ -14,9 +14,6 @@ class CursoType extends AbstractType
         $builder
             ->add('name')
             ->add('workload')
-            ->add('created_at')
-            ->add('updated_at')
-            ->add('studentCollection')
         ;
     }
 
