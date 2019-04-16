d3edca77fe07ada194f9a2a84bc370ee2a47d9d1
rosental <cristiane.rosental@gmail.com>
2019-04-16 00:22:00 -0300

    Atualizando AlunoConrtroller para novas configurações.

--- src/Controller/AlunoController.php
+++ src/Controller/AlunoController.php
@@ -9,97 +9,98 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Tests\Fixtures\AnnotationFixtures\AbstractClassController;
 
 /**
- * Class AlunoController
+ * Class StudentController
  * @package App\Controller
- * @Route("/aluno", name="aluno")
+ * @Route("/student")
  */
-
-class AlunoController extends AbstractClassController
+class AlunoController extends AbstractController
 {
     /**
-     * @param Response $request
+     * @param Request $request
      * @param null $id
      * @return Response
-     * @Route("/{id}", methods={"GET"}, name="aluno_get")
+     * @Route("/{id}", name="student_get", methods={"GET"})
      */
     public function getAction(Request $request, $id = null)
     {
-        $alunoData = $this->getDoctrine()->getRepository('App\Entity\Aluno');
-        if (is_null($id)) {
-            $alunoData = $alunoData->findAll();
-        } else {
-            $id = (int) $id;
-            $alunoData = $alunoData->find($id);
-        }
-        $aluno = SerializerBuilder::create()->build()->serialize($alunoData, 'json');
-        return new Response($aluno, 200, ['Content-Type' => 'application/json']);
+//        // $data = $request->request->all();
+//        $studentData = $this->getDoctrine()->getRepository('App\Entity\Student');
+//        if (is_null($id)) {
+//            $studentData = $studentData->findAll();
+//        } else {
+//            $id = (int) $id;
+//            $studentData = $studentData->find($id);
+//        }
+//        $student = SerializerBuilder::create()->build()->serialize($studentData, 'json');
+//        return new Response($student, 200, ['Content-Type' => 'application/json']);
     }
 
     /**
      * @param Request $request
      * @return JsonResponse
      * @throws \Exception
-     * @Route("", methods={"POST"}, name="aluno_post")
+     * @Route("", name="student_save", methods={"POST"})
      */
     public function postAction(Request $request)
     {
-        $data = $request->request->all();
-
-        $doctrine = $this->getDoctrine()->getManager();
-
-        $aluno = new Aluno();
-        $aluno->setNome($data['nome']);
-        $aluno->setCpf($data['cpf'])
-              ->setCep($data['cep'])
-              ->setEmail($data['email'])
-              ->setEndereco($data['endereco'])
-              ->setTelefone($data['telefone']);
-
-        $doctrine->persist($aluno);
-        $doctrine->flush();
-
-        return new JsonResponse(['msg' => 'Aluno inserido com sucesso!'], 200);
+//        $data = $request->request->all();
+//        $student = new Student();
+//        $form = $this->createForm(StudentType::class, $student);
+//        $form->submit($data);
+//        // $student->setName($data['name']);
+//        // $student->setCpf($data['cpf']);
+//        // $student->setAddress($data['address']);
+//        // $student->setCep($data['cep']);
+//        // $student->setEmail($data['email']);
+//        // $student->setPhoneNumber($data['phone_number']);
+//        // $student->setCourseCollection($data['course-collection']);
+//        // $student->setCreatedAt(new \DateTime("now", new \DateTimeZone("America/Sao_Paulo")));
+//        // $student->setUpdatedAt(new \DateTime("now", new \DateTimeZone("America/Sao_Paulo")));
+//        $doctrine = $this->getDoctrine()->getManager();
+//        $doctrine->persist($student);
+//        $doctrine->flush();
+//        return new JsonResponse(['msg' => 'Aluno inserido com sucesso!'], 200);
     }
 
     /**
      * @param Request $request
      * @return JsonResponse|\Symfony\Component\HttpKernel\Exception\NotFoundHttpException
-     * * @Route("", methods={"PUT"}, name="aluno_put")
+     * @throws \Exception
+     * @Route("", name="student_update", methods={"PUT"})
      */
     public function putAction(Request $request)
     {
-        $data = $request->request->all();
-        $aluno = $this->getDoctrine()->getRepository('App\Entity\Aluno')->find($data['id']);
-        $aluno->setNome($data['nome'])
-              ->setCpf($data['cpf'])
-              ->setEmail($data['email'])
-              ->setCep($data['cpf'])
-              ->setEndereco($data['endereco'])
-              ->setTelefone($data['telefone']);
-        if (!$aluno) {
-            return $this->createNotFoundException('Aluno Not Found!');
-        }
-
-        $doctrine = $this->getDoctrine()->getManager();
-        $doctrine->merge($aluno);
-        $doctrine->flush();
-
-        return new JsonResponse(["message" => "Aluno atualizado com sucesso."], 200);
+//        $data = $request->request->all();
+//        $student = $this->getDoctrine()->getRepository('App\Entity\Student')->find($data['id']);
+//        $form = $this->createForm(StudentType::class, $student);
+//        $form->submit($data);
+//        // $student->setCpf($data['cpf']);
+//        // $student->setAddress($data['address']);
+//        // $student->setCep($data['cep']);
+//        // $student->setEmail($data['email']);
+//        // $student->setPhoneNumber($data['phone_number']);
+//        // $student->setUpdatedAt(new \DateTime("now", new \DateTimeZone("America/Sao_Paulo")));
+//        if (!$student) {
+//            return $this->createNotFoundException('Student Not Found!');
+//        }
+//        $doctrine = $this->getDoctrine()->getManager();
+//        $doctrine->merge($student);
+//        $doctrine->flush();
+//        return new JsonResponse(["message" => "Aluno atualizado com sucesso."], 200);
     }
 
     /**
-     * @param Curso $aluno
+     * @param Student $student
      * @return JsonResponse
-     * @Route("/{id}", methods={"DELETE"}, name="aluno_delete")
+     * @Route("/{id}", name="student_delete", methods={"DELETE"})
      */
     public function deleteAction(Aluno $aluno)
     {
-        $doctrine = $this->getDoctrine()->getManager();
-        $doctrine->remove($aluno);
-        $doctrine->flush();
-        return new JsonResponse(['message' => 'Aluno Removido com Sucesso!'], 200);
+//        $doctrine = $this->getDoctrine()->getManager();
+//        $doctrine->remove($student);
+//        $doctrine->flush();
+//        return new JsonResponse(['message' => 'Aluno Removido com Sucesso!'], 200);
     }
 }
